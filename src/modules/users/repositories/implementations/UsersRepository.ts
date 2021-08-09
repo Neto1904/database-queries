ce15416bab382f4ddb1d6d1bb05ff56d508f274d
Neto1904 <netohsa66@gmail.com>
2021-08-09 13:54:37 -0300

    Complete Users Repository methods

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,20 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({ relations: ['games'], where: { id: user_id }})
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    const users = await this.repository.query('SELECT * FROM users ORDER BY first_name ASC'); // Complete usando raw query
+    return users
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const users = await this.repository.query(`SELECT email, first_name, last_name FROM users WHERE users.first_name ILIKE '${first_name}' AND users.last_name ILIKE '${last_name}'`); // Complete usando raw query
+    return users
   }
 }
